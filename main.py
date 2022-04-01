21f9bff99b1d451198dd9fed2e2f12985aa3099b
DouMaokang <alecdou0001@gmail.com>
2022-04-01 11:00:19 +0800

    save

---
+++ main.py
@@ -0,0 +1,21 @@
+import threading
+import logging
+import time
+
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    x = threading.Thread(target=thread_function, args=(1,), daemon=False)
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
